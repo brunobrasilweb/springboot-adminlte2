27c6624a506ba0047991cd2fcd6248e2f79ebdf8
Bruno Brasil <brunobrasilweb@gmail.com>
2016-06-30 23:01:24 -0300

    Removão de arquivos de testes

--- src/main/java/br/com/brunobrasilweb/springadm/MvcConfig.java
+++ src/main/java/br/com/brunobrasilweb/springadm/MvcConfig.java
@@ -9,8 +9,7 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/").setViewName("dashboard");
         registry.addViewController("/login").setViewName("login");
     }
 
