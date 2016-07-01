ac7813efb0a8cbaeffc1f831abfef2c0e71a0db1
Bruno Brasil <brunobrasilweb@gmail.com>
2016-06-30 23:02:01 -0300

    MvcConfig

---
+++ target/classes/sql/security.sql
@@ -0,0 +1,1 @@
+create table users (    username varchar(50) not null primary key,    password varchar(255) not null,    enabled boolean not null) engine = InnoDb;create table authorities (    username varchar(50) not null,    authority varchar(50) not null,    foreign key (username) references users (username),    unique index authorities_idx_1 (username, authority)) engine = InnoDb;
